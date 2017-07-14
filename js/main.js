7b6ac478bdb0880bc2220fb56d359a4587769e15
ValLR <valentinalopezrecabal@gmail.com>
2017-07-14 10:50:38 -0400

    funcion on click para salgo.html y rescate de api

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-	$(".button-collapse").sideNav();
-	$('.collapsible').collapsible();
-    $('select').material_select();
-    $("#logIn").click(function(e){
+
+    //validations for index.html
+    $("#logIn").click(function(){
     	if(($("#email").val())==""||(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test($("#email").val())===false)){
 
     		$("#correoForm").text("Debes ingresar un correo válido");
@@ -14,5 +13,36 @@ $(document).ready(function(){
     		$("#logIn").attr("href","opciones.html")
     	}
     })
+    localStorage.setItem('email',$("#email").val());
+    localStorage.setItem('nTarjeta', $("#password").val())
 
+    //email en profile, aún no corre bien
+    if($("#email").val() === !undefined){
+    	$("#email-p").attr("value", localStorage.getItem('email'));
+    }
+
+
+   //llamada de api
+   $("#saldo").on("click", function(){
+   	var id = ($("#nTarjetaSaldo").val());
+			$.ajax({
+				url:'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip='+id+'',
+				type: 'GET',
+				datatype: 'json',
+			})
+			.done(function(response){
+				console.log(response.saldoTarjeta);
+
+			})
+			.fail(function(){
+				console.log("error");
+			});
+		
+
+   });
+   
+	//plugins que inicializan complementos de materialize
+	$(".button-collapse").sideNav();
+	$('.collapsible').collapsible();
+    $('select').material_select();
 });
