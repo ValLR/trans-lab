735bf041640046b53ee9c4adbe193d367eff605b
ValLR <valentinalopezrecabal@gmail.com>
2017-07-14 04:08:13 -0400

    función de validación de password y desbloqueo de botón lista

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$('.collapsible').collapsible();
     $('select').material_select();
-    var email = $("#email").val();
-    var password = $("#password").val();
     $("#logIn").click(function(e){
-    	if(email==""||(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email))===false){
-    		$("#correoForm").text("Debes ingresar un correo válido")
+    	if(($("#email").val())==""||(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test($("#email").val())===false)){
+
+    		$("#correoForm").text("Debes ingresar un correo válido");
+    	}
+    	else if(($("#password").val()).length >= 8){
+    		$("#passwordForm").text("Tu contraseña no debe superar los 8 caracteres")
+    	}
+    	else{
+    		$("#logIn").attr("href","opciones.html")
     	}
     })
 
