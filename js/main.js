6188421f2e4d950a74018fd254e12a2fc8a430f8
ValLR <valentinalopezrecabal@gmail.com>
2017-07-14 03:10:11 -0400

    avances en la función de validación de correo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$('.collapsible').collapsible();
-
+    $('select').material_select();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    $("#logIn").click(function(e){
+    	if(email==""||(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email))===false){
+    		$("#correoForm").text("Debes ingresar un correo válido")
+    	}
+    })
 
 });
